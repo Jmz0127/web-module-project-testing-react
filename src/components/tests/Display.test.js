b5f84df37b5b2e0bcede647474d4883e16ccba68
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2021-12-13 21:56:21 -0500

    renders Show component when the button is clicked

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -4,11 +4,40 @@ import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
+import mockFetchShow from '../../api/fetchShow';
+
+jest.mock('../../api/fetchShow');
+
+const testShow = {
+	name: 'test show',
+	summary: 'test summary',
+	seasons: [
+		{
+			id: 0,
+			name: 'Season 1',
+			episodes: []
+		},
+		{
+			id: 1,
+			name: 'Season 2',
+			episodes: []
+		}
+	]
+};
 
 test('renders without errors with no props', () => {
 	render(<Display />);
 });
 
-test('renders Show component when the button is clicked ', () => {});
+test('renders Show component when the button is clicked ', async () => {
+	mockFetchShow.mockResolvedValueOnce(testShow);
+
+	render(<Display />);
+	const button = screen.getByRole('button');
+	userEvent.click(button);
+
+	const show = await screen.findByTestId('show-container');
+	expect(show).toBeInTheDocument();
+});
 
 test('renders show season options matching your data when the button is clicked', () => {});
